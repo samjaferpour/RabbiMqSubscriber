7b778018921d6cc2e5c9abf46c7e5decddcffbd6
s.jaferpour <s.jaferpour@JAFERPOUR-PC>
2023-02-08 11:06:42 +0330

    initial create

---
+++ RabbitMqSubsCriber/RabbitMqSubsCriber/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName= "localhost",
+    VirtualHost= "/",
+    UserName= "guest",
+    Password= "guest",
+    Port = 5672
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange: "chat.exchange", type: ExchangeType.Direct);
+channel.QueueDeclare(queue: "chat_queue", durable: true, autoDelete: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (ch, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var result = Encoding.UTF8.GetString(body);
+    Console.WriteLine(result);
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+channel.BasicConsume(queue: "chat_queue", autoAck: false, exclusive: false, consumer: consumer);
+Console.ReadLine();
